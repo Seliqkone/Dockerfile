a4ae4fa0fa75611b850d7b83ab893eb32586170d
Selik@kone <selikiekone@gmail.com>
2022-01-27 12:38:53 +0200

    initial commit

---
+++ docker_project.py
@@ -0,0 +1,23 @@
+projectName='Docker_Final_Project'
+password='docker_project'
+# print('Please guess a password!')
+# guess_a_password = input()
+if password == 'docker_project':
+ print('Ends of docker project.')
+if projectName == 'Docker_Final_Project':
+    print('Access granded ' + projectName + ' is completed.')
+else:
+    print('You must complete the project.')
+
+
+
+# name = 'Mary'
+#
+# password = 'swordfish'
+# if name == 'JOE':
+#
+#  print('Hello, Mary')
+# if password == 'sword':
+#     print('Access granted.')
+# else:
+#     print('Wrong password.')
